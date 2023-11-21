e7fd78c0b63ca6f491a0627963afa4fa754a6d45
clangoi <cagonzalez24@uc.cl>
2023-11-21 15:17:33 -0300

    commit

--- chatbot.py
+++ chatbot.py
@@ -117,7 +117,8 @@ if authentication_status == True:
         session_state.past = [] 
 
     def get_text():
-        input_text = st.text_area("Escribe tus consultas ", value="", help="Si ya apreció Running y no se actualizó la página, haga click nuevamente en el boton Enviar")
+        input_text = st.text_area("Escribe tus consultas ", "")
+        st.caption("Si ya apareció Running y no se actualizó la página, haga click nuevamente en el boton Enviar")
         return input_text
 
     user_input = get_text()
